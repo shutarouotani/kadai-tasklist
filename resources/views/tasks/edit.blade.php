baac43e7657a9137b2dee4bda3e5e5641be2f068
shutarou.ootani <shutarou5.otani7@gmail.com>
2018-09-03 11:40:59 +0000

    bootstrap design2

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,7 +5,7 @@
     <h1>＜ id =「{{ $task->id }}」の編集 ＞</h1>
     
     <div class="row">
-        <div class="col-xs-12 col-sm-6 col-sm-offset-2 col-md-6 col-md-offset-2 col-lg-3 col-lg-offset-3">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
                 
                 <div class="form-group">
